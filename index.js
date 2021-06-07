bd4f1c8a786aceba1ac4ebf5495d44521674edb5
Muhibullah Khan Kamali <57056374+Muhibullah-09@users.noreply.github.com>
2021-06-07 18:44:59 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const input = document.getElementById('input');
+// const value = input.value;
+// console.log(value)
+
+const Converter = () => {
+    const value = input.value
+    console.log(value)
+}
